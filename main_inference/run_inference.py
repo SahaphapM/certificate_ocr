0a166dfb84be942bbd87f15101f82081162616e4
SahaphapM <65160309@go.buu.ac.th>
2025-07-01 01:17:59 +0700

    fix: change structure

---
+++ main_inference/run_inference.py
@@ -0,0 +1,28 @@
+import os
+from model_loader import load_model
+from predictor import predict_entities
+from utils import print_results, save_to_json
+from data import sample_texts
+
+
+def main():
+    # เปลี่ยนเป็น path ของโมเดล
+    model_path = "trained_model"
+    model, tokenizer, device = load_model(model_path)
+
+    all_results = []
+    for i, text in enumerate(sample_texts, 1):
+        print(f"\n\n{'#' * 30} EXAMPLE {i} {'#' * 30}")
+        entities = predict_entities(text, model, tokenizer, device)
+        print_results(text, entities)
+        all_results.append({
+            "text": text,
+            "entities": entities
+        })
+
+    save_to_json(all_results, "main_inference/predictions.json")
+    print("\nPredictions saved to predictions.json")
+
+
+if __name__ == "__main__":
+    main()
